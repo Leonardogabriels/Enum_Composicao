5831880694a92cf54ff1f9025253001fd2c75830
leonardogabriels <leonardogabrielsilva1@outlook.com>
2023-08-28 14:50:06 -0300

    Worker Class Update

--- src/Entities/Worker.java
+++ src/Entities/Worker.java
@@ -1,6 +1,7 @@
 package Entities;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import Enumeracoes.WorkLevel;
@@ -22,6 +23,63 @@ public class Worker {
         this.baseSalary = baseSalary;
         this.department = department;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkLevel level) {
+        this.level = level;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void addContract(HourContract contract){
+        contracts.add(contract);
+    }
+    public void removeContract(HourContract contract){
+        contracts.remove(contract);
+    }
+    public double income(int year, int month){
+        double soma = baseSalary;
+        Calendar cal = Calendar.getInstance();
+        for(HourContract c: contracts){
+            cal.setTime(c.getData());
+            int c_year = cal.get(Calendar.YEAR);
+            int c_month = 1 + cal.get(Calendar.MONTH);
+            if(year == c_year && month == c_month){
+                soma += c.totalValue();
+            }
+        }
+        return soma;
+    }
+    
     
 
     
